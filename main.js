b5564d1e1d70a93928a7c845a2031e783e3c9160
Micha Schwab <me@michailschwab.com>
2019-01-19 15:24:08 -0500

    started the zoom out animation

--- main.js
+++ main.js
@@ -3,22 +3,33 @@ var PanZoomTrailer = /** @class */ (function () {
     function PanZoomTrailer() {
         this.width = 0;
         this.height = 0;
+        this.phoneHeightSvg = 165;
+        this.phoneHeightM = 0.15;
+        this.svgUnitToM = this.phoneHeightM / this.phoneHeightSvg;
+        this.initialBottomPadding = 330;
         var vis = document.getElementById('vis');
         if (!vis) {
             throw Error('Visualization DOM element not found');
         }
         this.vis = vis;
         this.setSize();
+        window.addEventListener('resize', this.setSize.bind(this));
         var hand = document.getElementById('hand');
         var phone = document.getElementById('phone');
         var text = document.getElementById('text');
-        if (!hand || !phone || !text) {
+        var heightLine = document.getElementById('height-line');
+        var heightEndMarker = document.getElementById('height-end-marker');
+        var perspective = document.getElementById('perspective');
+        if (!hand || !phone || !text || !heightLine || !heightEndMarker || !perspective) {
             throw new Error('Could not find crucial DOM elements');
         }
         this.hand = hand;
         this.phone = phone;
         this.text = text;
-        this.step0();
+        this.heightLine = heightLine;
+        this.heightEndMarker = heightEndMarker;
+        this.perspective = perspective;
+        this.step1();
     }
     PanZoomTrailer.prototype.setSize = function () {
         this.width = window.innerWidth;
@@ -26,12 +37,12 @@ var PanZoomTrailer = /** @class */ (function () {
         this.vis.setAttribute('width', this.width.toString());
         this.vis.setAttribute('height', this.height.toString());
     };
-    PanZoomTrailer.prototype.step0 = function () {
+    PanZoomTrailer.prototype.step1 = function () {
         var _this = this;
         var centerX = this.width / 2;
         var handFrom = { x: centerX + 100, y: this.height };
-        var handTo = { x: centerX - 90, y: this.height - 330 };
-        this.moveTo(this.phone, centerX - 100, this.height - 500);
+        var handTo = { x: centerX - 90, y: this.height - this.initialBottomPadding };
+        this.moveTo(this.phone, centerX - 100, this.height - this.initialBottomPadding - 170);
         this.moveTo(this.hand, handFrom.x, handFrom.y);
         this.text.innerText = 'Pan and Zoom is important because we use it a lot.';
         setTimeout(function () {
@@ -39,28 +50,113 @@ var PanZoomTrailer = /** @class */ (function () {
                 var interpolateFct = _this.getInterpolateChoordFct(progressPercent, handFrom, handTo);
                 _this.moveTo(_this.hand, interpolateFct('x'), interpolateFct('y'));
             }, function () {
-                _this.step1();
+                _this.moveTo(_this.hand, handTo.x, handTo.y);
+                _this.step2();
             }).start(500);
         }, 1000);
     };
-    PanZoomTrailer.prototype.step1 = function () {
+    PanZoomTrailer.prototype.step2 = function () {
         var _this = this;
         var centerX = this.width / 2;
-        var handFrom = { x: centerX - 90, y: this.height - 330 };
-        var handTo = { x: centerX - 90, y: this.height - 400 };
-        setTimeout(function () {
+        var handBottom = { x: centerX - 90, y: this.height - this.initialBottomPadding };
+        var handTop = { x: centerX - 90, y: this.height - this.initialBottomPadding - 70 };
+        var up = function (onEnd) {
             new PZTAnimation(function (progressPercent) {
-                var interpolateFct = _this.getInterpolateChoordFct(progressPercent, handFrom, handTo);
+                var interpolateFct = _this.getInterpolateChoordFct(progressPercent, handBottom, handTop);
                 _this.moveTo(_this.hand, interpolateFct('x'), interpolateFct('y'));
             }, function () {
+                onEnd();
             }).start(500);
+        };
+        var down = function (onEnd) {
+            new PZTAnimation(function (progressPercent) {
+                var interpolateFct = _this.getInterpolateChoordFct(progressPercent, handTop, handBottom);
+                _this.moveTo(_this.hand, interpolateFct('x'), interpolateFct('y'));
+            }, function () {
+                onEnd();
+            }).start(150);
+        };
+        setTimeout(function () {
+            up(function () { return down(function () { return up(function () { return down(function () { return up(function () { return down(function () {
+                _this.step3();
+            }); }); }); }); }); });
         }, 500);
     };
+    PanZoomTrailer.prototype.step3 = function () {
+        var _this = this;
+        this.text.innerText = 'In fact, this is how much you daily: ';
+        var centerX = this.width / 2;
+        var handBottom = { x: centerX - 90, y: this.height - this.initialBottomPadding };
+        var handTop = { x: centerX - 90, y: this.height - this.initialBottomPadding - 70 };
+        var lineX = centerX - 16;
+        var lineStartY = this.height - this.initialBottomPadding + 60;
+        var lineHeightSvgAddedPerCycle = 90;
+        var goalHeightM = 25;
+        var completedBarHeightSvg = 0;
+        var currentlyAddingBarHeightSvg = 0;
+        var scale = 1;
+        var updateBarHeight = function () {
+            var totalBarHeightSvg = completedBarHeightSvg + currentlyAddingBarHeightSvg;
+            var totalbarHeightM = Math.round(totalBarHeightSvg * _this.svgUnitToM);
+            _this.text.innerText = 'In fact, this is how much you daily: ' + totalbarHeightM + 'm';
+            _this.attr(_this.heightEndMarker, 'x1', lineX - 5 / scale);
+            _this.attr(_this.heightEndMarker, 'x2', lineX + 5 / scale);
+            _this.attr(_this.heightEndMarker, 'y1', lineStartY - totalBarHeightSvg);
+            _this.attr(_this.heightEndMarker, 'y2', lineStartY - totalBarHeightSvg);
+            _this.attr(_this.heightLine, 'x1', lineX);
+            _this.attr(_this.heightLine, 'x2', lineX);
+            _this.attr(_this.heightLine, 'y1', lineStartY);
+            _this.attr(_this.heightLine, 'y2', lineStartY - totalBarHeightSvg);
+        };
+        this.attr(this.perspective, 'transform-origin', lineX + 'px ' + (lineStartY + 200) + 'px');
+        var updateZoom = function () {
+            var totalBarHeightSvg = completedBarHeightSvg + currentlyAddingBarHeightSvg;
+            scale = Math.min(1, 1 / (totalBarHeightSvg / 300));
+            _this.attr(_this.perspective, 'transform', 'scale(' + scale + ')');
+            _this.attr(_this.heightEndMarker, 'stroke-width', 1 / scale);
+            _this.attr(_this.heightLine, 'stroke-width', 1 / scale);
+        };
+        var up = function (onEnd) {
+            new PZTAnimation(function (progressPercent) {
+                var interpolateFct = _this.getInterpolateChoordFct(progressPercent, handBottom, handTop);
+                _this.moveTo(_this.hand, interpolateFct('x'), interpolateFct('y'));
+                currentlyAddingBarHeightSvg = progressPercent * lineHeightSvgAddedPerCycle;
+                updateBarHeight();
+                updateZoom();
+            }, function () {
+                completedBarHeightSvg += lineHeightSvgAddedPerCycle;
+                currentlyAddingBarHeightSvg = 0;
+                onEnd();
+            }).start(30);
+        };
+        var down = function (onEnd) {
+            _this.moveTo(_this.hand, handBottom.x, handBottom.y);
+            setTimeout(onEnd);
+        };
+        var todo = [];
+        var countCyclesNeeded = Math.round(goalHeightM / lineHeightSvgAddedPerCycle / this.svgUnitToM);
+        for (var i_1 = 0; i_1 < countCyclesNeeded; i_1++) {
+            todo.push(up);
+            todo.push(down);
+        }
+        var i = -1;
+        var workTodo = function () {
+            i++;
+            if (todo[i]) {
+                todo[i](workTodo);
+            }
+        };
+        setTimeout(function () { return workTodo(); }, 1000);
+    };
+    ;
     PanZoomTrailer.prototype.getInterpolateChoordFct = function (progressPercent, from, to) {
         return function (coord) { return from[coord] + (to[coord] - from[coord]) * progressPercent; };
     };
     PanZoomTrailer.prototype.moveTo = function (element, x, y) {
         element.setAttributeNS(null, 'transform', 'translate(' + x + ', ' + y + ')');
+    };
+    PanZoomTrailer.prototype.attr = function (element, attrName, value) {
+        element.setAttributeNS(null, attrName, value.toString());
     };
     return PanZoomTrailer;
 }());
--- main.ts
+++ main.ts
@@ -7,6 +7,14 @@ class PanZoomTrailer {
     private text: HTMLElement;
     private hand: SVGElement;
     private phone: SVGElement;
+    private heightLine: SVGLineElement;
+    private heightEndMarker: SVGLineElement;
+    private perspective: SVGGElement;
+
+    private phoneHeightSvg = 165;
+    private phoneHeightM = 0.15;
+    private svgUnitToM = this.phoneHeightM / this.phoneHeightSvg;
+    private initialBottomPadding = 330;
 
     constructor() {
         const vis = document.getElementById('vis');
@@ -17,18 +25,26 @@ class PanZoomTrailer {
 
         this.vis = vis;
         this.setSize();
+        window.addEventListener('resize', this.setSize.bind(this));
 
         const hand = document.getElementById('hand');
         const phone = document.getElementById('phone');
         const text = document.getElementById('text');
-        if(!hand || !phone || !text) {
+        const heightLine = document.getElementById('height-line');
+        const heightEndMarker = document.getElementById('height-end-marker');
+        const perspective = document.getElementById('perspective');
+
+        if(!hand || !phone || !text || !heightLine || !heightEndMarker || !perspective) {
             throw new Error('Could not find crucial DOM elements');
         }
         this.hand = <SVGElement> <unknown> hand;
         this.phone = <SVGElement> <unknown> phone;
         this.text = text;
+        this.heightLine = <SVGLineElement> <unknown> heightLine;
+        this.heightEndMarker = <SVGLineElement> <unknown> heightEndMarker;
+        this.perspective = <SVGGElement> <unknown> perspective;
 
-        this.step0();
+        this.step1();
     }
 
     private setSize() {
@@ -38,39 +54,132 @@ class PanZoomTrailer {
         this.vis.setAttribute('height', this.height.toString());
     }
 
-    private step0() {
+    private step1() {
         const centerX = this.width / 2;
         const handFrom = {x: centerX + 100, y: this.height};
-        const handTo = {x: centerX - 90, y: this.height - 330};
+        const handTo = {x: centerX - 90, y: this.height - this.initialBottomPadding};
 
-        this.moveTo(this.phone, centerX - 100, this.height - 500);
+        this.moveTo(this.phone, centerX - 100, this.height - this.initialBottomPadding - 170);
         this.moveTo(this.hand, handFrom.x, handFrom.y);
 
         this.text.innerText = 'Pan and Zoom is important because we use it a lot.';
         setTimeout(() => {
             new PZTAnimation((progressPercent: number) => {
                 const interpolateFct = this.getInterpolateChoordFct(progressPercent, handFrom, handTo);
-                this.moveTo(this.hand, interpolateFct('x'), interpolateFct('y'))
+                this.moveTo(this.hand, interpolateFct('x'), interpolateFct('y'));
             }, () => {
-                this.step1();
+                this.moveTo(this.hand, handTo.x, handTo.y);
+                this.step2();
             }).start(500);
         }, 1000);
     }
 
-    private step1() {
+    private step2() {
         const centerX = this.width / 2;
-        const handFrom = {x: centerX - 90, y: this.height - 330};
-        const handTo = {x: centerX - 90, y: this.height - 400};
+        const handBottom = {x: centerX - 90, y: this.height - this.initialBottomPadding};
+        const handTop = {x: centerX - 90, y: this.height - this.initialBottomPadding - 70};
 
-        setTimeout(() => {
+        const up = (onEnd: () => void) => {
             new PZTAnimation((progressPercent: number) => {
-                const interpolateFct = this.getInterpolateChoordFct(progressPercent, handFrom, handTo);
+                const interpolateFct = this.getInterpolateChoordFct(progressPercent, handBottom, handTop);
                 this.moveTo(this.hand, interpolateFct('x'), interpolateFct('y'))
             }, () => {
-
+                onEnd();
             }).start(500);
+        };
+        const down = (onEnd: () => void) => {
+            new PZTAnimation((progressPercent: number) => {
+                const interpolateFct = this.getInterpolateChoordFct(progressPercent, handTop, handBottom);
+                this.moveTo(this.hand, interpolateFct('x'), interpolateFct('y'))
+            }, () => {
+                onEnd();
+            }).start(150);
+        };
+        setTimeout(() => {
+            up(() => down(() => up(() => down(() => up(() => down(() => {
+                this.step3();
+            }))))));
         }, 500);
     }
+
+    private step3() {
+        this.text.innerText = 'In fact, this is how much you daily: ';
+
+        const centerX = this.width / 2;
+        const handBottom = {x: centerX - 90, y: this.height - this.initialBottomPadding};
+        const handTop = {x: centerX - 90, y: this.height - this.initialBottomPadding - 70};
+        const lineX = centerX - 16;
+        const lineStartY = this.height - this.initialBottomPadding + 60;
+        const lineHeightSvgAddedPerCycle = 90;
+        const goalHeightM = 25;
+
+        let completedBarHeightSvg = 0;
+        let currentlyAddingBarHeightSvg = 0;
+        let scale = 1;
+
+        const updateBarHeight = () => {
+            const totalBarHeightSvg = completedBarHeightSvg + currentlyAddingBarHeightSvg;
+            const totalbarHeightM = Math.round(totalBarHeightSvg * this.svgUnitToM);
+
+            this.text.innerText = 'In fact, this is how much you daily: ' + totalbarHeightM + 'm';
+
+            this.attr(this.heightEndMarker, 'x1', lineX - 5 / scale);
+            this.attr(this.heightEndMarker, 'x2', lineX + 5 / scale);
+            this.attr(this.heightEndMarker, 'y1', lineStartY - totalBarHeightSvg);
+            this.attr(this.heightEndMarker, 'y2', lineStartY - totalBarHeightSvg);
+
+            this.attr(this.heightLine, 'x1', lineX);
+            this.attr(this.heightLine, 'x2', lineX);
+            this.attr(this.heightLine, 'y1', lineStartY);
+            this.attr(this.heightLine, 'y2', lineStartY - totalBarHeightSvg);
+        };
+
+        this.attr(this.perspective, 'transform-origin', lineX + 'px ' + (lineStartY + 200) + 'px');
+
+        const updateZoom = () => {
+            const totalBarHeightSvg = completedBarHeightSvg + currentlyAddingBarHeightSvg;
+            scale = Math.min(1, 1 / (totalBarHeightSvg / 300));
+
+            this.attr(this.perspective, 'transform', 'scale(' + scale + ')');
+            this.attr(this.heightEndMarker, 'stroke-width', 1 / scale);
+            this.attr(this.heightLine, 'stroke-width', 1 / scale);
+        };
+
+        const up = (onEnd: () => void) => {
+            new PZTAnimation((progressPercent: number) => {
+                const interpolateFct = this.getInterpolateChoordFct(progressPercent, handBottom, handTop);
+                this.moveTo(this.hand, interpolateFct('x'), interpolateFct('y'));
+                currentlyAddingBarHeightSvg = progressPercent * lineHeightSvgAddedPerCycle;
+                updateBarHeight();
+                updateZoom();
+            }, () => {
+                completedBarHeightSvg += lineHeightSvgAddedPerCycle;
+                currentlyAddingBarHeightSvg = 0;
+                onEnd();
+            }).start(30);
+        };
+        const down = (onEnd: () => void) => {
+            this.moveTo(this.hand, handBottom.x, handBottom.y);
+            setTimeout(onEnd);
+        };
+
+        const todo: ((cb: () => void) => void)[] = [];
+        const countCyclesNeeded = Math.round(goalHeightM / lineHeightSvgAddedPerCycle / this.svgUnitToM);
+
+        for(let i = 0; i < countCyclesNeeded; i++) {
+            todo.push(up);
+            todo.push(down);
+        }
+
+        let i = -1;
+        const workTodo = () => {
+            i++;
+            if(todo[i]) {
+                todo[i](workTodo);
+            }
+        };
+        setTimeout(() => workTodo(), 1000);
+    };
 
     private getInterpolateChoordFct(progressPercent: number, from: {x: number, y: number}, to: {x: number, y: number}) {
         return (coord: 'x'|'y') => from[coord] + (to[coord] - from[coord]) * progressPercent;
@@ -78,6 +187,10 @@ class PanZoomTrailer {
 
     private moveTo(element: SVGElement, x: number, y: number) {
         element.setAttributeNS(null, 'transform', 'translate(' + x + ', ' + y + ')');
+    }
+
+    private attr(element: SVGElement, attrName: string, value: string|number) {
+        element.setAttributeNS(null, attrName, value.toString());
     }
 }
 
